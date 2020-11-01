bb25dfbc8199f4814001c592aa5ee180aeac7bc8
Roma Sagar <Roma Sagar@LAPTOP-OIRER49H>
2020-11-01 14:03:27 +0530

    creat new print class

---
+++ GitHub/src/git/PrintClass.java
@@ -0,0 +1,12 @@
+package git;
+
+public class PrintClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is my first class for git repositries");
+
+	}
+
+}
